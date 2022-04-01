a7fe68ca2292b9f3e83862f9887dce3df4c5301a
Matias Molina <molina.matias.87@gmail.com>
2022-04-01 17:06:49 -0300

    inicio1

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
+//const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen(3000, () => console.log("Servidor corriendo puerto 3000"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+});
