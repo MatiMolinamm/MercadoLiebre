445895524aebdd7409504695876092c7fc8204b2
Matias Molina <molina.matias.87@gmail.com>
2022-04-04 23:05:01 -0300

    inicio mercado liebre

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-//const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(path.resolve(__dirname, "./public")));
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log("Servidor corriendo puerto 3000"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
